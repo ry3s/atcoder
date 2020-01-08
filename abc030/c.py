4c98bfbd1849ba048cf34a5cb3622cba66e0d807
ry3s <ktchg752@yahoo.co.jp>
2020-01-08 19:50:26 +0900

    update

--- abc030/c.py
+++ abc030/c.py
@@ -1,4 +1,25 @@
+import bisect
+
 n, m = map(int, input().split())
 x, y = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
+
+time = 0
+lp = 0
+
+while True:
+    id = bisect.bisect_left(a, time)
+    if id >= n:
+        break
+
+    time = a[id] + x
+
+    id = bisect.bisect_left(b, time)
+    if id >= m:
+        break
+
+    time = b[id] + y
+    lp += 1
+
+print(lp)
---
+++ abc030/d.py
@@ -0,0 +1,5 @@
+n, a = map(int, input().split())
+k = int(input().split())
+b = list(map(int, input().split()))
+
+while
