7ef9d1f0eab5fbea50e1f99f537ce44fd7e3a848
ry3s <ktchg752@yahoo.co.jp>
2020-04-10 04:06:37 +0900

    language update

---
+++ judge_update_202004/b.rs
@@ -0,0 +1,65 @@
+use std::io::{stdin, Read, StdinLock};
+use std::str::FromStr;
+
+#[allow(dead_code)]
+struct Scanner<'a> {
+    cin: StdinLock<'a>,
+}
+#[allow(dead_code)]
+impl<'a> Scanner<'a> {
+    fn new(cin: StdinLock<'a>) -> Scanner<'a> {
+        Scanner { cin: cin }
+    }
+
+    fn read<T: FromStr>(&mut self) -> Option<T> {
+        let token = self.cin.by_ref().bytes().map(|c| c.unwrap() as char)
+            .skip_while(|c| c.is_whitespace())
+            .take_while(|c| !c.is_whitespace())
+            .collect::<String>();
+        token.parse::<T>().ok()
+    }
+
+    fn input<T: FromStr>(&mut self) -> T {
+        self.read().unwrap()
+    }
+
+    fn vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.input()).collect()
+    }
+
+    fn mat<T: FromStr>(&mut self, row: usize, col: usize) -> Vec<Vec<T>> {
+        (0..row).map(|_| self.vec(col)).collect()
+    }
+}
+
+use std::collections::BTreeSet;
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    let n: usize = sc.input();
+    let arr: Vec<(usize, char)> = (0..n)
+        .map(|_| (sc.input(), sc.input()))
+        .collect();
+
+    let mut blue = BTreeSet::new();
+    let mut red = BTreeSet::new();
+    for x in &arr {
+        if x.1 == 'R' {
+            red.insert(x.0);
+        } else {
+            blue.insert(x.0);
+        }
+    }
+
+    let mut blue = blue.iter();
+    let mut red = red.iter();
+    for _ in 0..n {
+        if let Some(x) = red.next() {
+            println!("{}", x);
+        } else {
+            println!("{}", blue.next().unwrap());
+        }
+    }
+}
---
+++ judge_update_202004/c.rs
@@ -0,0 +1,143 @@
+use std::io::{stdin, Read, StdinLock};
+use std::str::FromStr;
+
+#[allow(dead_code)]
+struct Scanner<'a> {
+    cin: StdinLock<'a>,
+}
+#[allow(dead_code)]
+impl<'a> Scanner<'a> {
+    fn new(cin: StdinLock<'a>) -> Scanner<'a> {
+        Scanner { cin: cin }
+    }
+
+    fn read<T: FromStr>(&mut self) -> Option<T> {
+        let token = self.cin.by_ref().bytes().map(|c| c.unwrap() as char)
+            .skip_while(|c| c.is_whitespace())
+            .take_while(|c| !c.is_whitespace())
+            .collect::<String>();
+        token.parse::<T>().ok()
+    }
+
+    fn input<T: FromStr>(&mut self) -> T {
+        self.read().unwrap()
+    }
+
+    fn vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.input()).collect()
+    }
+
+    fn mat<T: FromStr>(&mut self, row: usize, col: usize) -> Vec<Vec<T>> {
+        (0..row).map(|_| self.vec(col)).collect()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    let a: Vec<i64> = sc.vec(3);
+    let n = a.iter().sum();
+    let mut arr: Vec<i64> = (1..=n).collect();
+
+    let mut ans = 0;
+    loop {
+        let mut x = [[0; 3]; 3];
+        let mut k = 0;
+        for i in 0..3 {
+            for j in 0..(a[i] as usize) {
+                x[i][j] = arr[k];
+                k += 1;
+            }
+        }
+        let mut ok = true;
+        for i in 0..3 {
+            for j in 0..(a[i] as usize) {
+                ok = ok
+                    && (i == 0 || x[i][j] > x[i - 1][j])
+                    && (j == 0 || x[i][j] > x[i][j - 1]);
+            }
+        }
+        if ok {
+            ans += 1;
+        }
+        if !arr.next_permutation() {
+            break;
+        }
+    }
+    println!("{}", ans);
+}
+
+pub trait LexicalPermutation {
+    /// Return `true` if the slice was permuted, `false` if it is already
+    /// at the last ordered permutation.
+    fn next_permutation(&mut self) -> bool;
+    /// Return `true` if the slice was permuted, `false` if it is already
+    /// at the first ordered permutation.
+    fn prev_permutation(&mut self) -> bool;
+}
+
+impl<T> LexicalPermutation for [T] where T: Ord {
+    /// Original author in Rust: Thomas Backman <serenity@exscape.org>
+    fn next_permutation(&mut self) -> bool {
+        // These cases only have 1 permutation each, so we can't do anything.
+        if self.len() < 2 { return false; }
+
+        // Step 1: Identify the longest, rightmost weakly decreasing part of the vector
+        let mut i = self.len() - 1;
+        while i > 0 && self[i-1] >= self[i] {
+            i -= 1;
+        }
+
+        // If that is the entire vector, this is the last-ordered permutation.
+        if i == 0 {
+            return false;
+        }
+
+        // Step 2: Find the rightmost element larger than the pivot (i-1)
+        let mut j = self.len() - 1;
+        while j >= i && self[j] <= self[i-1]  {
+            j -= 1;
+        }
+
+        // Step 3: Swap that element with the pivot
+        self.swap(j, i-1);
+
+        // Step 4: Reverse the (previously) weakly decreasing part
+        self[i..].reverse();
+
+        true
+    }
+
+    fn prev_permutation(&mut self) -> bool {
+        // These cases only have 1 permutation each, so we can't do anything.
+        if self.len() < 2 { return false; }
+
+        // Step 1: Identify the longest, rightmost weakly increasing part of the vector
+        let mut i = self.len() - 1;
+        while i > 0 && self[i-1] <= self[i] {
+            i -= 1;
+        }
+
+        // If that is the entire vector, this is the first-ordered permutation.
+        if i == 0 {
+            return false;
+        }
+
+        // Step 2: Reverse the weakly increasing part
+        self[i..].reverse();
+
+        // Step 3: Find the rightmost element equal to or bigger than the pivot (i-1)
+        let mut j = self.len() - 1;
+        while j >= i && self[j-1] < self[i-1]  {
+            j -= 1;
+        }
+
+        // Step 4: Swap that element with the pivot
+        self.swap(i-1, j);
+
+        true
+    }
+
+}
