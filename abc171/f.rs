48f9d7eab4e87446186a53530c99c624ca45c000
Shun <ktchg752@yahoo.co.jp>
2020-06-23 03:18:53 +0900

    update template.rs

--- abc171/f.rs
+++ abc171/f.rs
@@ -31,7 +31,7 @@ impl<'a> Scanner<'a> {
         (0..row).map(|_| self.vec(col)).collect()
     }
 }
-
+const MOD: usize = 1_000_000_007;
 fn main() {
     let cin = stdin();
     let cin = cin.lock();
@@ -39,6 +39,64 @@ fn main() {
 
     let k: usize = sc.input();
     let s: String = sc.input();
-    let chs: Vec<char> = s.chars().collect();
-    const MOD: usize = 1_000_000_007;
+    let n = s.len();
+
+    let max_n = 3_000_000;
+    let mut pow25 = vec![1usize; max_n];
+    let mut pow26 = vec![1usize; max_n];
+    for i in 0..max_n - 1 {
+        pow25[i + 1] = pow25[i] * 25 % MOD;
+        pow26[i + 1] = pow26[i] * 26 % MOD;
+    }
+
+    let com = Combination::new(max_n, MOD);
+
+    let mut ans: usize = 0;
+    for i in 0..=k {
+        let mut tmp = com.get(n - 1 + k - i, n - 1) * pow26[i] % MOD;
+        tmp = tmp * pow25[k - i];
+        tmp = tmp % MOD;
+        ans += tmp;
+        ans = ans % MOD;
+    }
+    println!("{}", ans);
+}
+
+struct Combination {
+    fac: Vec<usize>,
+    finv: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    fn new(max_n: usize, modulo: usize) -> Combination {
+        let mut fac = vec![0; max_n];
+        let mut finv = vec![0; max_n];
+        let mut inv = vec![0; max_n];
+        fac[0] = 1;
+        fac[1] = 1;
+        finv[0] = 1;
+        finv[1] = 1;
+        inv[1] = 1;
+        for i in 2..max_n {
+            fac[i] = fac[i - 1] * i % modulo;
+            inv[i] = modulo - inv[modulo % i] * (modulo / i) % modulo;
+            finv[i] = finv[i - 1] * inv[i] % modulo;
+        }
+        Combination {
+            fac: fac,
+            finv: finv,
+            modulo: modulo,
+        }
+    }
+
+    fn get(&self, n: usize, k: usize) -> usize {
+        assert!(n >= k);
+        self.fac[n] * (self.finv[k] * self.finv[n - k] % self.modulo) % self.modulo
+    }
+
+    #[allow(dead_code)]
+    fn h(&self, n: usize, k: usize) -> usize {
+        self.get(n + k - 1, k)
+    }
 }
--- template.rs
+++ template.rs
@@ -19,11 +19,7 @@ impl<'a> Scanner<'a> {
     }
 
     fn read<T: FromStr>(&mut self) -> Option<T> {
-        let token = self
-            .cin
-            .by_ref()
-            .bytes()
-            .map(|c| c.unwrap() as char)
+        let token = self.cin.by_ref().bytes().map(|c| c.unwrap() as char)
             .skip_while(|c| c.is_whitespace())
             .take_while(|c| !c.is_whitespace())
             .collect::<String>();
@@ -350,6 +346,44 @@ where
         true
     }
 }
+struct Combination {
+    fac: Vec<usize>,
+    finv: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    fn new(max_n: usize, modulo: usize) -> Combination {
+        let mut fac = vec![0; max_n];
+        let mut finv = vec![0; max_n];
+        let mut inv = vec![0; max_n];
+        fac[0] = 1;
+        fac[1] = 1;
+        finv[0] = 1;
+        finv[1] = 1;
+        inv[1] = 1;
+        for i in 2..max_n {
+            fac[i] = fac[i - 1] * i % modulo;
+            inv[i] = modulo - inv[modulo % i] * (modulo / i) % modulo;
+            finv[i] = finv[i - 1] * inv[i] % modulo;
+        }
+        Combination {
+            fac: fac,
+            finv: finv,
+            modulo: modulo,
+        }
+    }
+
+    fn get(&self, n: usize, k: usize) -> usize {
+        assert!(n >= k);
+        self.fac[n] * (self.finv[k] * self.finv[n - k] % self.modulo) % self.modulo
+    }
+
+    #[allow(dead_code)]
+    fn h(&self, n: usize, k: usize) -> usize {
+        self.get(n + k - 1, k)
+    }
+}
 
 #[test]
 fn lexical() {
@@ -369,55 +403,6 @@ fn lexical() {
     }
     assert_eq!(c, 5);
 }
-
-struct Combination {
-    max_n: usize,
-    fac: Vec<usize>,
-    finv: Vec<usize>,
-    inv: Vec<usize>,
-}
-
-impl Combination {
-    const MOD: usize = 1_000_000_007;
-    fn new(max_n: usize) -> Combination {
-        let mut fac = vec![0; max_n];
-        let mut finv = vec![0; max_n];
-        let mut inv = vec![0; max_n];
-        fac[0] = 1;
-        fac[1] = 1;
-        finv[0] = 1;
-        finv[1] = 1;
-        inv[1] = 1;
-        for i in 2..max_n {
-            fac[i] = fac[i - 1] * i % MOD;
-            inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-            finv[i] = finv[i - 1] * inv[i] % MOD;
-        }
-        Combination {
-            max_n: max_n,
-            fac: fac,
-            finv: finv,
-            inv: inv,
-        }
-    }
-
-    fn com(&self, n: usize, k: usize) -> usize {
-        if n < k || n < 0 || k < 0 {
-            0
-        } else {
-            self.fac[n] * (self.finv[k] * self.finv[n - k] % MOD) % MOD
-        }
-    }
-
-    fn hcom(&self, n: usize, k: usize) -> usize {
-        if n == 0 && k == 0 {
-            1
-        } else {
-            self.com(n + k - 1, k)
-        }
-    }
-}
-
 struct Bit {
     // Binary Indexed Tree (1-indexed)
     n: usize,
