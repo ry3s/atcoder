d114ed386a1d9a74d7eb82926804f3ee4898ef3a
Shun <ktchg752@yahoo.co.jp>
2020-07-11 15:28:23 +0900

    update

---
+++ past3/b.rs
@@ -0,0 +1,66 @@
+use std::io::{stdin, Read, StdinLock};
+use std::str::FromStr;
+
+#[allow(dead_code)]
+struct Scanner<'a> {
+    cin: StdinLock<'a>,
+}
+#[allow(dead_code)]
+impl<'a> Scanner<'a> {
+    fn new(cin: StdinLock<'a>) -> Scanner<'a> {
+        Scanner { cin: cin }
+    }
+
+    fn read<T: FromStr>(&mut self) -> Option<T> {
+        let token = self.cin.by_ref().bytes().map(|c| c.unwrap() as char)
+            .skip_while(|c| c.is_whitespace())
+            .take_while(|c| !c.is_whitespace())
+            .collect::<String>();
+        token.parse::<T>().ok()
+    }
+
+    fn input<T: FromStr>(&mut self) -> T {
+        self.read().unwrap()
+    }
+
+    fn vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.input()).collect()
+    }
+
+    fn mat<T: FromStr>(&mut self, row: usize, col: usize) -> Vec<Vec<T>> {
+        (0..row).map(|_| self.vec(col)).collect()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    let n: usize = sc.input();
+    let m: usize = sc.input();
+    let q: usize = sc.input();
+
+    let mut solved: Vec<Vec<usize>> = vec![vec![]; n];
+    let mut problems = vec![n; m];
+    for _ in 0..q {
+        let x: usize = sc.input();
+        match x {
+            1 => {
+                let y: usize = sc.input();
+                let mut score = 0;
+                for i in &solved[y - 1] {
+                    score += problems[i - 1];
+                }
+                println!("{}", score);
+            }
+            2 => {
+                let y: usize = sc.input();
+                let z: usize = sc.input();
+                problems[z - 1] -= if problems[z - 1] < 1 { 0 } else { 1 };
+                solved[y - 1].push(z);
+            }
+            _ => unreachable!()
+        }
+    }
+}
